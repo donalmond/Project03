1760576480d154109d4324dcdcb5d962b2d568cd
Tom Ulman <Tom@Toms-MacBook-Pro.local>
2016-05-11 18:47:58 +1000

    fuck turbolinks

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,14 +1,18 @@
+
+var testvar = 1;
 $(function(){
 
   $('#sendvote').click(function(e){
     e.preventDefault();
     // alert($(this).attr('post_id'));
+    console.log(  '/posts/' + $(this).attr('post_id') + '/vote'  );
     $.ajax({
       url: '/posts/' + $(this).attr('post_id') + '/vote',
       dataType: 'json',
     }).done(function(data){
       // handle ajax response
       console.log(data.vote);
+
     });
 
   });
